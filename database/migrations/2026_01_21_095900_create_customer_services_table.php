765ed295928f894c1b9b36b356ef80ea81f33dbe
Firza155 <cobalagi567@gmail.com>
2026-01-21 17:04:27 +0700

    Menambahkan migration tabel user, projects, lead, products, customers, customer_services

---
+++ database/migrations/2026_01_21_095900_create_customer_services_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customer_services', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('customer_id')
+                ->constrained()
+                ->cascadeOnDelete();
+
+            $table->foreignId('product_id')
+                ->constrained()
+                ->restrictOnDelete();
+
+            $table->date('start_date');
+            $table->enum('status', ['active', 'inactive', 'terminated'])
+                ->default('active');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customer_services');
+    }
+};
